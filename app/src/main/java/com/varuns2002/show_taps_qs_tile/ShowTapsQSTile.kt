e8c1f4b42068babda88499d5da1d8a857f07c0ac
VarunS2002 <30069380+VarunS2002@users.noreply.github.com>
2021-07-08 22:17:39 +0530

    Added main functionality code

--- app/src/main/java/com/varuns2002/show_taps_qs_tile/ShowTapsQSTile.kt
+++ app/src/main/java/com/varuns2002/show_taps_qs_tile/ShowTapsQSTile.kt
@@ -1,7 +1,50 @@
 package com.varuns2002.show_taps_qs_tile
 
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import android.provider.Settings.SettingNotFoundException
 import android.service.quicksettings.TileService
 
 class ShowTapsQSTile : TileService() {
+    override fun onClick() {
+        super.onClick()
+        if (!Settings.System.canWrite(this)) {
+            val intent = Intent("android.settings.action.MANAGE_WRITE_SETTINGS")
+            intent.data = Uri.parse("package:$packageName")
+            startActivity(intent)
+            return
+        }
+        val showTapsEnabled: Boolean = try {
+            Settings.System.getInt(contentResolver, "show_touches") == 1
+        } catch (e: SettingNotFoundException) {
+            e.printStackTrace()
+            false
+        }
+        if (!showTapsEnabled) {
+            Settings.System.putInt(contentResolver, "show_touches", 1)
+            qsTile.state = 2
+        } else {
+            Settings.System.putInt(contentResolver, "show_touches", 0)
+            qsTile.state = 1
+        }
+        qsTile.updateTile()
+    }
 
+    override fun onStartListening() {
+        super.onStartListening()
+        val showTapsEnabled: Boolean = try {
+            Settings.System.getInt(contentResolver, "show_touches") == 1
+        } catch (e: SettingNotFoundException) {
+            e.printStackTrace()
+            false
+        }
+        if (showTapsEnabled) {
+            qsTile.state = 2
+        } else {
+            qsTile.state = 1
+        }
+        qsTile.updateTile()
+
+    }
 }
